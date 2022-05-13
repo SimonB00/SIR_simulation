898d345303042a1b46f68a9955305570c5920640
Simone Balducci <93096843+SimonB00@users.noreply.github.com>
2022-05-13 08:43:16 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,19 +15,19 @@ int main() {
   std::string risp;
   std::string name;
 
-  std::cout << "Quante pandemie vuoi testare?" << '\n';
+  std::cout << "How many pandemics do you want to test?" << '\n';
   std::cin >> count;
 
   for (int i = 0; i < count; ++i) {
-    std::cout << "Vuoi usare una pandemia predefinita?" << '\n';
+    std::cout << "Do you want to use on of the premade pandemics?" << '\n';
     std::cin >> risp;
 
-    if (risp == "si") {
-      std::cout << "Inserisci il luogo della pandemia (Vaticano, Bologna, "
-                   "NewYork, Riccione, Ferrara, Londra)"
+    if (risp == "yes") {
+      std::cout << "Insert the city where the pandemics takes place (Vatican, Bologna, "
+                   "NewYork, Riccione, Ferrara, London)"
                 << '\n';
       std::cin >> name;
-      if (name == "Vaticano") {
+      if (name == "Vatican") {
         Vaticano(I_0, beta, gamma, n);
       } else if (name == "Bologna") {
         Bologna(I_0, beta, gamma, n);
@@ -37,18 +37,18 @@ int main() {
         Riccione(I_0, beta, gamma, n);
       } else if (name == "Ferrara") {
         Ferrara(I_0, beta, gamma, n);
-      } else if (name == "Londra") {
+      } else if (name == "London") {
         Londra(I_0, beta, gamma, n);
       }
 
     } else if (risp == "no") {
-      std::cout << "Inserire I_0: " << '\n';
+      std::cout << "Insert I_0: " << '\n';
       std::cin >> I_0;
-      std::cout << "Inserire beta: " << '\n';
+      std::cout << "Insert beta: " << '\n';
       std::cin >> beta;
-      std::cout << "Inserire gamma: " << '\n';
+      std::cout << "Insert gamma: " << '\n';
       std::cin >> gamma;
-      std::cout << "Inserire n: " << '\n';
+      std::cout << "Insert n: " << '\n';
       std::cin >> n;
     }
 
